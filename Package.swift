0111d15ccfd6fab1fbebb3e9b6e227110992fdb1
Devon Martin <devon.martin.re@gmail.com>
2023-10-10 15:16:03 -0400

    Possibly temporary update to a dependency.

--- Package.swift
+++ Package.swift
@@ -7,18 +7,14 @@ let package = Package(
     name: "Tiktoken",
     platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Tiktoken",
             targets: ["Tiktoken"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-		.package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "1.0.0")),
+		.package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "2.5.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Tiktoken",
             dependencies: [
